caf31f0fa5e35661d29e2aeb72d07e008b72830a
zaklance <wosewick@gmail.com>
2024-04-25 15:43:23 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const toDo = document.querySelector('#create-task-form');
+  toDo.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputValue = e.target['new-task-description'].value;
+    const list = document.querySelector('#tasks');
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.textContent = 'x'
+    li.textContent = inputValue;
+    list.append(li);
+    li.appendChild(btn);
+  });
+  // toRemove = document.querySelector('#tasks.li.btn');
+  // toRemove.addEventListener('click', (e) => {
+    // e.preventDefault();
+    // console.log('clicky');
+  // });
 });
